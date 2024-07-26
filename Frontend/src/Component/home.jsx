e95d9f7825fbaba91c7a66e2253fdae347ef5bec
himani bhimate <himanibhimate0808@gmail.com>
2024-07-26 21:17:57 +0530

    changes in home

--- Frontend/src/Component/home.jsx
+++ Frontend/src/Component/home.jsx
@@ -1,83 +1,59 @@
-import "../assets/css/home.css"
+import "../assets/css/home.css";
 import Carouselslid from "./Carousels";
 import room1 from "../assets/img/room-1.jpg.webp";
 import arrow from "../assets/img/arrowline.webp";
 import img1 from "../assets/img/home-midimg-1.webp";
 import img2 from "../assets/img/home-midimg-2.webp";
+
 const Home = () => {
     return (
-
-        // <div >
-        //     <section className="Main-div">
-        //         <div style={{display:"flex"}}>
-        //             <div style={{margin:"20px"}}>
-        //                 <h1>100% Satusfaction Gurantee</h1>
-        //                 <h2>Find Your <br />
-        //                 Perfect Course</h2>
-        //                 <p>Choose from over 210,000 online video courses with new additions <br /> Published every month ourses and learn how to code.</p>
-        //             </div>
-        //             <div>
-        //                 <div>
-        //                     <img src="" alt="" />
-        //                     <img src="" alt="" />
-        //                 </div>
-        //                 <div></div>
-        //             </div>
-        //         </div>
-        //     </section>
-
-
         <section>
-
-            <div className="row" style={{ display: "flex", marginRight: "auto", marginLeft: "auto", position: "relative", margin: "80px" }}>
-                <div className="col-md">
-                    <div style={{ position: "relative", top: "0px", left: "275px" }}>
-                        <img src={img1} alt="" style={{ height: "350px", width: "400px", borderRadius: "40px", top: "30px" }} />
-                        <img src={arrow} alt="" />
-
+            {/* <div className="row main-content">
+                <div className="col-md img-column">
+                    <div className="img-container">
+                        <img src={img1} alt="Mid Image 1" className="main-img" />
+                        <img src={arrow} alt="Arrow" className="arrow-img" />
                     </div>
-
-                    <div style={{ position: "absolute", zIndex: "-1", top: "138px" }}>
-                        <img src={img2} alt="" style={{ width: "500px", borderRadius: "40px" }} />
-
+                    <div className="img-background">
+                        <img src={img2} alt="Mid Image 2" className="background-img" />
                     </div>
                 </div>
-                <div className="col-md" style={{ marginLeft: "300px" }}>
-                    <h3 style={{ color: "green", fontFamily: "cursive", fontSize: "xx-large" }}>Guaranteed & Certified</h3>
-                    <h1 style={{ marginLeft: "30", fontSize: "58px" }}>The Worlds Best Online Education Insititude</h1>
-                    <p style={{ fontSize: "26px", color: "#2f4f4f" }}>Choose from over 210,000 online video courses with new Additions published every month</p>
+                <div className="col-md text-container">
+                    <h3 className="guarantee-text">Guaranteed & Certified</h3>
+                    <h1 className="main-heading">The World's Best Online Education Institute</h1>
+                    <p className="main-description">Choose from over 210,000 online video courses with new additions published every month.</p>
                 </div>
-            </div>
+            </div> */}
             <Carouselslid />
-
-            <div>
-                <div class="header">
+            <div className="apartment-section">
+                <div className="header">
                     <h1>Apartment Room</h1>
                 </div>
-                <div class="content">
-                    <div class="row">
-                        <img src={room1} alt="Room Image" class="room-image" />
-                        <div class="description">
+                <div className="content">
+                    <div className="rowmain">
+                        <img src={room1} alt="Room" className="room-image" />
+                        <div className="description">
                             <p>Hello</p>
                         </div>
-                        <img src={room1} alt="Room Image" class="room-image" />
-                        <div class="description">
+                        <img src={room1} alt="Room" className="room-image" />
+                        <div className="description">
                             <p>Hello</p>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="description">
+                    <div className="rowmain">
+                        <div className="description">
                             <p>Hello</p>
                         </div>
-                        <img src={room1} alt="Room Image" class="room-image" />
-                        <div class="description">
+                        <img src={room1} alt="Room" className="room-image" />
+                        <div className="description">
                             <p>Hello</p>
                         </div>
-                        <img src={room1} alt="Room Image" class="room-image" />
+                        <img src={room1} alt="Room" className="room-image" />
                     </div>
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
+
 export default Home;
