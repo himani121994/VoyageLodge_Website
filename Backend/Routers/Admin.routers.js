6db8c5229021eadb179673c743fcddf1cc8794a1
himani bhimate <himanibhimate0808@gmail.com>
2024-07-27 22:13:42 +0530

    add controllter for carousels

--- Backend/Routers/Admin.routers.js
+++ Backend/Routers/Admin.routers.js
@@ -2,6 +2,7 @@ const express = require('express');
 const adminController = require('../Controllers/Admin.Controller');
 const router = express.Router();
 
+router.post('/carousels', adminController.Carousels)
 router.post('/uploadroom', adminController.UploadRoom);
 router.post('/uploadvehicle',adminController.UploadVehicle);
 router.get('/allrooms', adminController.Allrooms)
--- Frontend/src/Dashboard/DashComponent/Carousels.jsx
+++ Frontend/src/Dashboard/DashComponent/Carousels.jsx
@@ -1,17 +1,31 @@
 import { useEffect,useState } from "react";
 import axios from "axios";
-import "../DashAsssets/css/carousels.css"
+import "../DashAsssets/css/carousels.css";
+
 const DashCarousels = ()=>{
+   const [sliderinput,setSliderinput] = useState([]);
+
+   const sliderSubmit =()=>{
+    let url = "http://localhost:8000/dashboard/carousels";
+    axios.post(url, sliderinput)
+     .then((res) => {
+      console.log(res.data);
+    }).catch((error) => {
+      console.error("There was an error uploading the room!", error);
+    });
+   }
+
     return(
         <div className="main-div">
           <form action="">
         
             <label htmlFor="">hadding</label>
-            <input type="text" name="hadding" />
+            <input type="text" name="hadding" onChange={e => setSliderinput(e.target.value)} />
             <label htmlFor="">Description</label>
-            <input type="text" name="description" />
+            <input type="text" name="description" onChange={e => setSliderinput(e.target.value)} />
             <label htmlFor="">image</label>
-            <input type="file" name="image" />
+            <input type="file" name="image" onChange={e => setSliderinput(e.target.value)} />
+            <button onClick={sliderSubmit}>submit</button>
           </form>
 
           
