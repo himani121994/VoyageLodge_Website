8d352480b97dd9d63fe733e85e1c57bcd64db5b9
himani bhimate <himanibhimate0808@gmail.com>
2024-08-03 22:47:45 +0530

    add user icon in topnav

--- Frontend/src/LayoutComponent/Topnav.jsx
+++ Frontend/src/LayoutComponent/Topnav.jsx
@@ -1,34 +1,33 @@
 import React from "react";
 import "./topnav.css";
 import logo from "../assets/img/logo.avif"
+import { FaRegUser } from "react-icons/fa";
 
-const Topnav =()=>{
-
-
-    return(
+const Topnav = () => {
+    return (
         <nav>
             <div className="main-nav-div">
                 <div className="logo-div">
-                    <a href="#" style={{textDecoration:"none"}}><img src={logo} alt="logo" className="logo-img"/>
-                    <span className="logo-span" >VoyageLodge</span></a>
+                    <a href="#" style={{ textDecoration: "none" }}>
+                        <img src={logo} alt="logo" className="logo-img" />
+                        <span className="logo-span">VoyageLodge</span>
+                    </a>
                     <h3>HOTEL</h3>
                 </div>
-                
                 <div className="search-div">
                     <input
                         className="search-topnav-input"
                         type="text"
                         placeholder="Search..."
-                        // value={searchQuery}
-                        // onChange={(e) => setSearchQuery(e.target.value)}
-                        // onKeyPress={handleKeyPress} 
                     />
-                    {/* <button onClick={handleSearch}>Search</button> */}
-                    <button >Search</button>
+                    <button>Search</button>
                 </div>
-                
+                <div className="user-div">
+                    <FaRegUser className="user" />
+                </div>
             </div>
         </nav>
-    )
-}
+    );
+};
+
 export default Topnav;
